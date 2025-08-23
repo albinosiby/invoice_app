c5bfe3a5d38afeff4182ebd19cc5b6e18b810a28
Albino Siby <150587794+albinosiby@users.noreply.github.com>
2025-08-23 18:20:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
             theme: lightTheme,
             darkTheme: darkTheme,
             themeMode: state.themeMode, // Set themeMode from the cubit's state
-            home: Mainscreen(),
+            home: Firstscreen(),
           );
         },
       ),
